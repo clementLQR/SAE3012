a0792fac0b89a78b443b75164bc360067347e5bd
clement <clement.lequeurre@gmail.com>
2025-11-29 17:42:16 +0100

    mise à jour des templates

--- modele/modele.php
+++ modele/modele.php
@@ -1,5 +1,11 @@
 <?php
 require 'connection.php';
 
-
+function get_all_categorie(){
+    global $mysqli;
+    $query = "Select * from categorie";
+    $result = mysqli_query($mysqli, $query);
+    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $categories;
+}
 ?>
