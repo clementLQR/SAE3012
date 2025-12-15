2b8e4495feb54421764d2ce4dd30cd2d97296d4a
cloe <cloe.dechamp@gmail.com>
2025-12-15 14:59:23 +0100

    css responsive fini

--- script/pageConnexionInscription.js
+++ script/pageConnexionInscription.js
@@ -6,19 +6,15 @@
 
         formulaires.forEach(form => form.style.display = 'none');
 
-        // Retirer la classe active de tous les boutons
         buttons.forEach(btn => btn.classList.remove('active'));
 
-        // Afficher la section correspondante
         const targetId = button.getAttribute('data-target');
         document.getElementById(targetId).style.display = 'block';
 
-        // Mettre le bouton en actif
         button.classList.add('active');
         });
     });
 
-    // Afficher le premier texte par défaut
     buttons[0].click();
 
     const form = document.getElementById('formInscription');
@@ -27,7 +23,7 @@
 
     form.addEventListener('submit', function(e) {
         if (mdp.value !== confirmation.value) {
-            e.preventDefault(); // Empêche l'envoi
+            e.preventDefault(); 
             console.log("Les mots de passe ne correspondent pas.");
             alert("Les mots de passe ne correspondent pas.");
         }
